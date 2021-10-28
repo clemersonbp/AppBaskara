d506445ccdc41f16676dcea6b9f5c3a4cc015e04
clemersonbp <clemersonpessalli@live.com>
2021-10-27 21:31:09 -0300

    Second commit - Layout

---
+++ app/src/main/java/com/example/appbaskara/Dados.java
@@ -0,0 +1,14 @@
+package com.example.appbaskara;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Dados extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dados);
+    }
+}
